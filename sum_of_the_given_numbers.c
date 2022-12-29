96526c0bf631b654c0948a7794613ac3abd820cb
veenamvenkatasravani <119730610+veenamvenkatasravani@users.noreply.github.com>
2022-12-29 19:21:16 +0530

    #include<stdio.h> int main() {     int tc,k;     scanf("%d",&tc);     for(k=1;k<=tc;k++)     {         int n,m,sum;         scanf("%d%d",&n,&m);         sum=m+n;         printf("%d",sum);         printf(" ");     } }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int tc,k;
+    scanf("%d",&tc);
+    for(k=1;k<=tc;k++)
+    {
+        int n,m,sum;
+        scanf("%d%d",&n,&m);
+        sum=m+n;
+        printf("%d",sum);
+        printf("
+");
+    }
+}
