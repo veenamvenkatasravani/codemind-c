4f299a0283a255741466cfd5a2291f2097fada02
veenamvenkatasravani <119730610+veenamvenkatasravani@users.noreply.github.com>
2022-12-30 21:03:09 +0530

    #include<stdio.h> int main() {     int ti,k;     scanf("%d",&ti);     for(k=1;k<=ti;k++)     {         int i,n,rev=0,old;         scanf("%d",&n);         old=n;         while(n>0)         {             i=n%10;             rev=rev*10+i;             n=n/10;         }         if(old==rev)         printf("True ");         else         printf("False ");     } }

---
+++ Palindrome_Number.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int ti,k;
+    scanf("%d",&ti);
+    for(k=1;k<=ti;k++)
+    {
+        int i,n,rev=0,old;
+        scanf("%d",&n);
+        old=n;
+        while(n>0)
+        {
+            i=n%10;
+            rev=rev*10+i;
+            n=n/10;
+        }
+        if(old==rev)
+        printf("True
+");
+        else
+        printf("False
+");
+    }
+}
