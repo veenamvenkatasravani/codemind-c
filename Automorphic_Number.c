c9d8966bbe4fce5377b7e0c806e6ffc8eb30d52d
veenamvenkatasravani <119730610+veenamvenkatasravani@users.noreply.github.com>
2023-05-30 20:09:14 +0530

    #include<stdio.h> int main() {     long int n,sqr,rev1=0,rev2=0,m,sount=0,count=0;     scanf("%ld",&n);     sqr=n*n;     while(n>0)     {         m=n%10;         sount++;         rev1=(rev1*10)+m;         n/=10;     }     while(sqr>0)     {         m=sqr%10;         count++;         rev2=(rev2*10)+m;         if(sount==count)         {             break;         }         sqr/=10;     }     if(rev1==rev2)     {         printf("Automorphic Number");     }     else     {         printf("Not an Automorphic Number");     }     return 0; }

--- Automorphic_Number.c
+++ Automorphic_Number.c
@@ -1,15 +1,34 @@
 #include<stdio.h>
-#include<math.h>
 int main()
 {
-    int n,sq,p,d,k;
-    scanf("%d",&n);
-    d=log10(n)+1;
-    sq=n*n;
-    p=pow(10,d);
-    k=sq%p;
-    if(k==n)
-    printf("Automorphic Number");
+    long int n,sqr,rev1=0,rev2=0,m,sount=0,count=0;
+    scanf("%ld",&n);
+    sqr=n*n;
+    while(n>0)
+    {
+        m=n%10;
+        sount++;
+        rev1=(rev1*10)+m;
+        n/=10;
+    }
+    while(sqr>0)
+    {
+        m=sqr%10;
+        count++;
+        rev2=(rev2*10)+m;
+        if(sount==count)
+        {
+            break;
+        }
+        sqr/=10;
+    }
+    if(rev1==rev2)
+    {
+        printf("Automorphic Number");
+    }
     else
-    printf("Not an Automorphic Number");
+    {
+        printf("Not an Automorphic Number");
+    }
+    return 0;
 }
