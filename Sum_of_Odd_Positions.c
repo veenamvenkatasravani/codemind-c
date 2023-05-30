40514a6ca04882a347610a09c6ddc39566a3a695
veenamvenkatasravani <119730610+veenamvenkatasravani@users.noreply.github.com>
2023-05-30 20:05:09 +0530

    #include<stdio.h> #include<stdlib.h> int main() {     int arr[100],i,n,sum=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(i%2!=0)         {             sum=sum+arr[i];         }
        }
        printf("%d",sum);
    }

---
+++ Sum_of_Odd_Positions.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int arr[100],i,n,sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i%2!=0)
+        {
+            sum=sum+arr[i];
+        }
+        
+    }
+    printf("%d",sum);
+}
